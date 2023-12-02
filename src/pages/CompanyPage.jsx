ab8abff907f399ca14120154db236bb258f60d4a
Juan Rassa <jmrassa.dev@gmail.com>
2023-12-02 16:44:37 +0000

    Complete Iteration 4

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom';
+
 function CompanyPage() {
   return (
     <div>
+      <Link to='/'>← Go Back</Link>
       <h1>Company Profile</h1>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
-function HomePage() {
+import { Link } from 'react-router-dom';
+function HomePage({ companiesData }) {
+  console.log(companiesData);
   return (
     <div>
       <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      <div className='companies-container'>
+        {companiesData.map(company => {
+          return (
+            <Link to={`/company/${company.name}`} className='company-card' key={company.id}>
+              <h3>{company.name}</h3>
+              <img className='company-logo' src={company.logo} alt={`${company.name} logo`} />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
