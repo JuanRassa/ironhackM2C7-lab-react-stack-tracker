d51a1b4caa95130bbd70ab12b4b79a53d46db317
Juan Rassa <jmrassa.dev@gmail.com>
2023-12-02 15:09:50 +0000

    Complete Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import "./App.css";
+import { useState } from 'react';
+
+import './App.css';
+import companies from './companies.json';
+import technologies from './technologies.json';
 
 function App() {
+  const [companiesData, setCompaniesData] = useState(companies);
+  const [technologiesData, setTechnologiesData] = useState(technologies);
+
+  console.log('<()> App info: ', [companiesData, technologiesData]);
   return (
-    <div className="App">
+    <div className='App'>
       <h1>LAB | React Stack Tracker</h1>
     </div>
   );
