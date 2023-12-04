0d35a80976d98238b54bcb993a9464ccf7571551
Juan Rassa <jmrassa.dev@gmail.com>
2023-12-04 19:48:14 +0000

    Complete Iteration 5.3.

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -4,7 +4,7 @@ function CompanyPage({ companiesData }) {
   const { companySlug } = useParams();
 
   const companyInfo = companiesData.find(company => company.name === companySlug);
-
+  console.log(companyInfo);
   return (
     <div>
       <Link className='go-back-btn' to='/'>
@@ -14,14 +14,30 @@ function CompanyPage({ companiesData }) {
       <div className='company-container'>
         {!companyInfo && <h3>No Company was found :(</h3>}
         {companyInfo && (
-          <div className='company-info-card'>
-            <img className='company-info-card-logo' src={companyInfo.logo} alt={companyInfo.name}></img>
-            <div className='company-about'>
-              <h4>{companyInfo.name}</h4>
-              <h5>About</h5>
-              <p>{companyInfo.description}</p>
+          <>
+            <div className='company-info-card'>
+              <img className='company-info-card-logo' src={companyInfo.logo} alt={companyInfo.name}></img>
+              <div className='company-about'>
+                <h4>{companyInfo.name}</h4>
+                <h5>About</h5>
+                <p>{companyInfo.description}</p>
+              </div>
             </div>
-          </div>
+            <div className='company-stack-container'>
+              <div className='company-stack-wrapper '>
+                {companyInfo.techStack.map(stack => {
+                  return (
+                    <Link to={`/tech/${stack.slug}`} className='company-stack-link' key={stack.name}>
+                      <div className='company-stack-image-container'>
+                        <img src={stack.image} alt={stack.name} />
+                      </div>
+                      <h4 className='company-stack-name'>{stack.name}</h4>
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
